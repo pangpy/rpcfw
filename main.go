40864a5cbe8b691ad495e7892622655bb1eba8bc
pangpy <643813301@qq.com>
2023-09-08 21:47:09 +0800

    grpc-server

--- main.go
+++ main.go
@@ -3,27 +3,30 @@ package main
 import (
 	"context"
 	"fmt"
-	pb "github.com/pangpy/rpcfw/calculator"
 	"google.golang.org/grpc"
 	"log"
 	"net"
+	pb "rpcfw/proto"
 )
 
-type server struct{}
+type server struct {
+	pb.UnimplementedCalculatorServer
+}
 
 func (s *server) Add(ctx context.Context, in *pb.AddRequest) (*pb.AddResponse, error) {
+	// 在这里实现 Add 方法的逻辑
 	result := in.A + in.B
 	return &pb.AddResponse{Result: result}, nil
 }
 
 func main() {
-	lis, err := net.Listen("tcp", ":50051")
+	lis, err := net.Listen("tcp", ":50059")
 	if err != nil {
 		log.Fatalf("Failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
 	pb.RegisterCalculatorServer(s, &server{})
-	fmt.Println("Server is running on port 50051...")
+	fmt.Println("Server is running on port 50059...")
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("Failed to serve: %v", err)
 	}
